ded1acf795374dd8ebea5ec395e0c57a626b2305
Fer Torres <fertorresmx@gmail.com>
2020-11-04 00:06:03 -0600

    Preparing to Heroku

--- server.js
+++ server.js
@@ -14,17 +14,31 @@ app.use(compression());
 app.use(express.json());
 
 app.use('/', routes); // to use the routes
+// Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/index.html');
+});
 
 mongoose.connect(
-  process.env.MONGODB_URI, 
-  { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true},
+  process.env.MONGODB_URI,
+  {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } },
+  },
     function (err) {
       if (err) return console.log("Error: ", err);
-      console.log("MongoDB connection -- Ready state is:",
-      mongoose.connection.readyState);
+      console.log(
+        "MongoDB connection -- Ready state is:",
+        mongoose.connection.readyState
+      );
     }
 );
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('App is listening on port ' + listener.address().port)
-})
+});
